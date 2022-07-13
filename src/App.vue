3eaf561afdcdbe883f40ca34cf2aeb1527cd6780
Kyle Hernandez <kh2kt@kylesdev91yahoo.onmicrosoft.com>
2022-07-13 09:55:07 -0500

    View Cart takes you to your cart

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
   <div>
-    <header>{{ cart.length }} in cart</header>
-    <h1>Products</h1>
-    <div class="products">
-      <div v-for="product in products" :key="product.name">
-        {{ product.name }}
-        <img :src="product.image" />
-        <div>{{ product.cost }}</div>
-        <button v-on:click="addItemToCart(product)">Add to cart</button>
+    <header>
+      {{ cart.length }} in cart
+      <button v-on:click="navigateTo('cart')">View Cart</button>
+    </header>
+
+    <div v-if="page === 'cart'">
+      <h1>Your Cart</h1>
+    </div>
+
+    <div v-if="page === 'products'">
+      <h1>Products</h1>
+      <div class="products">
+        <div v-for="product in products" :key="product.name">
+          {{ product.name }}
+          <img :src="product.image" />
+          <div>{{ product.cost }}</div>
+          <button v-on:click="addItemToCart(product)">Add to cart</button>
+        </div>
       </div>
     </div>
   </div>
@@ -18,6 +28,7 @@ export default {
   name: 'App',
   data: () => {
     return {
+      page: 'products',
       cart: [],
       products: [
         {
@@ -30,7 +41,7 @@ export default {
           name: 'Cookies',
           cost: '$5.99',
           image:
-            'https://www.inkatrinaskitchen.com/wp-content/uploads/2012/03/Rainbow-Chocolate-Chip-Cookies-Recipe-In-Katrinas-Kitchen-600.jpg',
+            'https://www.recipetineats.com/wp-content/uploads/2017/06/Soft-Chocolate-Chip-Cookies-3.jpg?w=500&h=500&crop=1',
         },
       ],
     };
@@ -39,6 +50,9 @@ export default {
     addItemToCart(product) {
       this.cart.push(product);
       console.log(this.cart);
+    },
+    navigateTo(page) {
+      this.page = page;
     },
   },
   components: {},
@@ -57,7 +71,11 @@ body {
 }
 
 header {
-  height: 100px;
-  background-color: #ddd;
+  height: 60px;
+  background-color: #eee;
+  box-shadow: 2px 2px 5px #999;
+  text-align: right;
+  font-size: 30px;
+  padding-top: 20px;
 }
 </style>
